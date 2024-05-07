ea79bec24fc9667e5e72a9270e8820b70606c656
unknown <dominik.majdandzic@gmail.com>
2024-05-07 14:34:10 +0200

    Added code for properly loading all products on page before collecting product info

--- sports-nutrition-shop-api/src/main/java/com/dmajd/nutritionapi/service/ScraperService.java
+++ sports-nutrition-shop-api/src/main/java/com/dmajd/nutritionapi/service/ScraperService.java
@@ -2,8 +2,15 @@ package com.dmajd.nutritionapi.service;
 
 import jakarta.annotation.PostConstruct;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ScraperService
@@ -26,9 +33,35 @@ public class ScraperService
     public void scrape()
     {
         driver.get(URL);
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        System.out.println(driver.findElement(By.className("product-item-click")).getAttribute("title"));
-        System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+        // set implicit wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        try
+        {
+            // click button to load all products and wait
+            WebElement showMoreBtn = driver.findElement(By.className("amscroll-load-button"));
+            showMoreBtn.click();
+
+            // wait some time for elements to load
+            TimeUnit.SECONDS.sleep(3);
+
+            // get all products
+            WebElement mainContent = driver.findElement(By.id("amasty-shopby-product-list"));
+            List<WebElement> products = mainContent.findElements(By.className("product-item-click"));
+
+            for(WebElement product : products)
+            {
+                System.out.println(product.getAttribute("title"));
+            }
+
+        } catch (NoSuchElementException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        catch (Exception e)
+        {
+            System.out.println("An error occured");
+            System.out.println(e.getMessage());
+        }
         driver.quit();
     }
 }
