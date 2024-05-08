89e15a61baea540869e944bf4f5c716dedc3b83c
unknown <dominik.majdandzic@gmail.com>
2024-05-08 12:27:41 +0200

    Added code for scraping the relevant content of the page

--- sports-nutrition-shop-api/src/main/java/com/dmajd/nutritionapi/service/PolleoScraperService.java
+++ sports-nutrition-shop-api/src/main/java/com/dmajd/nutritionapi/service/PolleoScraperService.java
@@ -1,7 +1,15 @@
 package com.dmajd.nutritionapi.service;
 
+import jakarta.annotation.PostConstruct;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class PolleoScraperService implements ScraperService
@@ -9,6 +17,12 @@ public class PolleoScraperService implements ScraperService
     private static final String URL = "https://polleosport.hr/proteini/whey-protein/";
 
     private final FirefoxDriver driver;
+
+    @PostConstruct
+    void postConstruct()
+    {
+        scrape();
+    }
 
     PolleoScraperService(FirefoxDriver driver)
     {
@@ -18,7 +32,45 @@ public class PolleoScraperService implements ScraperService
     @Override
     public void scrape()
     {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+        driver.get(URL);
+
+        try
+        {
+            // close cookies window
+            driver.findElement(By.id("gdpr-cookie-accept")).click();
+            // wait some time for cookies window to close
+            TimeUnit.SECONDS.sleep(2);
+
+            // close promotion window if it exists
+            if (driver.findElement(By.id("myModal")) != null)
+            {
+                driver.findElement(By.id("myModal")).findElement(By.className("close")).click();
+
+                // wait some time for promotion window to close
+                TimeUnit.SECONDS.sleep(2);
+            }
+
+            // select option to display all products
+            Select numOfProducts = new Select(driver.findElement(By.id("input-limit")));
+            numOfProducts.selectByValue("100");
+            // wait some time for products to load
+            TimeUnit.SECONDS.sleep(3);
+
+            // get main content
+            WebElement mainContent = driver.findElement(By.id("main"));
+            System.out.println(mainContent.getText());
+
+        } catch (NoSuchElementException e)
+        {
+            System.out.println(e.getMessage());
+        } catch (Exception e)
+        {
+            System.out.println("An error occurred");
+            System.out.println(e.getMessage());
+        }
+        driver.quit();
     }
 
     @Override
