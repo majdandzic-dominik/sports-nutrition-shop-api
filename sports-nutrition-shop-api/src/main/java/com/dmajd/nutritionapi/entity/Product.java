63df5836922e9dcfc98f4260510fdb953f49b8c7
unknown <dominik.majdandzic@gmail.com>
2024-05-11 20:38:23 +0200

    Added class for product

---
+++ sports-nutrition-shop-api/src/main/java/com/dmajd/nutritionapi/entity/Product.java
@@ -0,0 +1,53 @@
+package com.dmajd.nutritionapi.entity;
+
+public class Product
+{
+    private String name;
+    private String price;
+    private String amount;
+    private boolean isAvailable;
+
+    public Product()
+    {
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(String price)
+    {
+        this.price = price;
+    }
+
+    public String getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(String amount)
+    {
+        this.amount = amount;
+    }
+
+    public boolean isAvailable()
+    {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available)
+    {
+        isAvailable = available;
+    }
+}
